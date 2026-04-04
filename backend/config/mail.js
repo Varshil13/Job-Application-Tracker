8c361465a1023aef1463ea01e4e5d205385f84ca
Varshil13 <sharmapriyanshu91@gmail.com>
2026-04-04 23:03:46 +0530

    Fixed mail configs

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,15 +1,16 @@
    
    const nodemailer = require("nodemailer");
    
-   const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS,
-      },
-      connectionTimeout: 15000,
-      greetingTimeout: 10000,
-      socketTimeout: 20000,
-    });
-
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // REQUIRED
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS, // MUST be app password
+  },
+  connectionTimeout: 20000,
+  greetingTimeout: 15000,
+  socketTimeout: 30000,
+});
     module.exports = transporter;
